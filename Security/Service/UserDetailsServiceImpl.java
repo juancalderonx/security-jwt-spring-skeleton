8860b089e2af9fab1feaf5b1cca5588ff1705f3c
juancalderonx <juanescalderon12@gmail.com>
2022-05-29 17:09:02 -0500

    Skeleton

---
+++ Security/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package Package.Security.Service;
+
+import Package.Entities.User;
+import Package.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * Autentica un usuario de la base de datos
+ *
+ * Authentication Manager llama al método loadUserByUsername de esta clase
+ * para obtener los detalles del usuario de la base de datos cuando
+ * se intente autenticar un usuario
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),user.getPassword(),new ArrayList<>());
+    }
+}
